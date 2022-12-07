f9e8ecbe55d68ed4a7efe8f4226dccfb11eacea2
JoseTorres022 <joseangel.tosa@outlook.com>
2022-12-06 20:11:36 -0700

    mostrar propiedades de ejemplo OK

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,29 @@ include '../includes/templates/header.php';
     <?php endif; ?>
 
     <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen de referencia</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casas en la playa</td>
+                <td> <img src="/bienesraices/imagenes/21cd53e0b28a1dd36d4a02ecb6d48c00.jpg" class="imagen-tabla" alt=""> </td>
+                <td>120000</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 
